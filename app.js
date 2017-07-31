9f856290885c7d4d2307eb40c7cb2fc93602cd20
TIEMPODEV\Francisco Ochoa <fochoa@tiempodevelopment.com>
2017-07-31 14:12:05 -0700

    cors stuff

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ var cors = require('cors');
 mongoose.connect(config.database);
 
 app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.set('superSecret', config.secret);
